6a7cb5ee91940066cf199e92e4651a5aac995441
王文辉 <wangwenhui@gehua.cc>
2023-03-09 17:51:50 +0800

    update

--- src/api/uploadFile.js
+++ src/api/uploadFile.js
@@ -6,12 +6,11 @@ class upFile {
   async upfile(ctx) {
     const file = ctx.request.files.file;
     const basename = path.basename(file.path);
-    ctx.body = {"url": `${ctx.origin}/${basename}`}
+    ctx.body = {"url": `${ctx.origin}/${basename}`} 
   }
   
   async getfile(ctx) {
     const files = fs.readdirSync(path.join(__dirname, '../../static'), { encoding: 'utf8', widthFileTypes: true })
-    console.log(files);
     let fileArr = []
     files.forEach(item=>{
       const basename = path.basename(item);
